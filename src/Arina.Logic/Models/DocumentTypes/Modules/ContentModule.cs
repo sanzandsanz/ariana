d6ad5199dac4859d25f7cdac39ca946d9c014530
sanjay <Sanjay.Bajracharya@deepend.com.au>
2018-08-17 16:00:58 +1000

    Added neste content module

---
+++ src/Arina.Logic/Models/DocumentTypes/Modules/ContentModule.cs
@@ -0,0 +1,13 @@
+﻿using Our.Umbraco.Ditto;
+using Umbraco.Core.Models;
+using System.Web;
+
+namespace Arina.Logic.Models
+{
+    public class ContentModule 
+    {
+        public virtual string Title { get; set; }
+
+        public virtual HtmlString Content { get; set; }
+    }
+}
--- src/Arina.Logic/Models/DocumentTypes/Pages/Home.cs
+++ src/Arina.Logic/Models/DocumentTypes/Pages/Home.cs
@@ -1,7 +1,10 @@
-﻿namespace Ariana.Logic.Models
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+
+namespace Ariana.Logic.Models
 {
     public class Home
     {
-
+        public IEnumerable<IPublishedContent> Modules { get; set; }
     }
 }
