115f31b8633a689d86cc24df4de02e22503ac99f
ARIANA-WORKSTAT\Ariana <sanjay.bajracharya4@gmail.com>
2018-08-30 22:23:17 +1000

    Add NuPickerEnumAttribute class

---
+++ src/Ariana.Umbraco.CMS/ComponentModel/Attributes/Processors/NuPickerEnumAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace Ariana.Umbraco.ComponentModel
+{
+    using System;
+
+    using nuPickers;
+
+    using Our.Umbraco.Ditto;
+
+    /// <summary>
+    /// Provides a unified way of converting the <see cref="Picker"/> type to the given enum. />
+    /// </summary>
+    public class NuPickerEnumAttribute : DittoMultiProcessorAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NuPickerEnumAttribute"/> class.
+        /// </summary>
+        public NuPickerEnumAttribute()
+            : base(new DittoProcessorAttribute[] { new NuPickerEnumConverterAttribute(), new EnumAttribute() })
+        {
+        }
+
+        /// <summary>
+        /// Returns the correct value to pass to the <see cref="EnumAttribute"/>
+        /// </summary>
+        private class NuPickerEnumConverterAttribute : DittoProcessorAttribute
+        {
+            /// <inheritdoc/>
+            public override object ProcessValue()
+            {
+                Picker picker = this.Value as Picker;
+                return picker != null ? picker.SavedValue : this.Value;
+            }
+        }
+    }
+}
