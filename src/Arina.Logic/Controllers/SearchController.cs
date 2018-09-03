9234619f9164dd9ce2eedcec055c2af7a00eaf13
sanjay <Sanjay.Bajracharya@deepend.com.au>
2018-09-03 17:34:51 +1000

    Added sample search page with document type

---
+++ src/Arina.Logic/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Ariana.Logic.Models;
+using Umbraco.Web.Mvc;
+using Our.Umbraco.Ditto;
+using Umbraco.Web.Models;
+
+
+namespace Arina.Controllers
+{
+    public class SearchController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            Search page = model.As<Search>();
+            return this.View("Search", page);
+        }
+    }
+}
